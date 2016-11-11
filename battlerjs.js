ceef5ba1e5678eac7b2a5fe0d0f1feaf4ce19f90
James Sibert <jsibert@basecampcodingacademy.org>
2016-11-11 15:42:06 -0600

    images

--- battlerjs.js
+++ battlerjs.js
@@ -1,19 +1,3 @@
-// peach
-// rosalina
-// zelda
-// samus
-// donald
-// goofy
-// doctor strange
-// joker
-// captain america
-// thor
-// loki
-// wolverine
-// finn
-// sephiroth
-
-
 // start with just undertale and avengers, then add others
 
 // character: image, name, number, alliance
