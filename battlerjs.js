98389832766563bc0140b5eb2108ca29803ff511
James Sibert <jsibert@basecampcodingacademy.org>
2016-11-11 09:15:12 -0600

    added images

--- battlerjs.js
+++ battlerjs.js
@@ -14,6 +14,19 @@
 // sora
 // donald
 // goofy
+// doctor strange
+// spider man
+// joker
+// hulk
+// captain america
+// thor
+// loki
+// wolverine
+// finn
+// iron man
+
+
+// start with just undertale and avengers, then add others
 
 // character: image, name, number, alliance
 // if character > other character:
